1f8c17e25fecd64c954c39d18e04c1dd5d0fbc53
nixos-goddess <nixosgoddess@transfem.org>
2023-12-14 18:04:44 -0700

    give the bootloader a timeout time

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,9 +11,12 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.loader = {
+    systemd-boot.enable = true;
+    timeout = 5;
+    efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot";
+  };
   boot.kernelParams = [ "module_blacklist=i915" ];
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_6;
 
